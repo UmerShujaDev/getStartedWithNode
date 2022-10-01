71d79774eec80afa4a0edcc0139a675d12795d9b
UmerShuja12 <suirsahDev@gmail.com>
2022-10-02 01:19:45 +0500

    exports and module.export

---
+++ modules.js
@@ -0,0 +1,10 @@
+// console.log(arguments);
+// console.log(require('module').wrapper);
+
+// const Calculator = require('./module-test-1');
+
+// const cal1 = new Calculator();
+// console.log(cal1.add(2, 3));
+
+const { name } = require('./module-test-1');
+console.log('🚀 ~ file: modules.js ~ line 10 ~ name', name);
