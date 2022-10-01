d36be49afd6febb5b0a81baa360369a43430b5de
UmerShuja12 <suirsahDev@gmail.com>
2022-10-02 01:25:01 +0500

    modules for caching

---
+++ module-test-2.js
@@ -0,0 +1,3 @@
+console.log('Hello from the module test 2');
+
+module.exports = () => console.log('Hi beautiful');
--- modules.js
+++ modules.js
@@ -6,5 +6,11 @@
 // const cal1 = new Calculator();
 // console.log(cal1.add(2, 3));
 
-const { name } = require('./module-test-1');
-console.log('🚀 ~ file: modules.js ~ line 10 ~ name', name);
+// const { name } = require('./module-test-1');
+// console.log('🚀 ~ file: modules.js ~ line 10 ~ name', name);
+
+// caching
+require('./module-test-2')();
+require('./module-test-2')();
+require('./module-test-2')();
+require('./module-test-2')();
